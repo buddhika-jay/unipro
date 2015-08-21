27dab5d1318e725ccf9b82d7373599322998a39d
buddhika-jay <buddhika.12@cse.mrt.ac.lk>
2015-08-21 13:50:20 +0530

    database configured

---
+++ src/UniproBundle/Entity/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: buddhika
+ * Date: 8/21/15
+ * Time: 12:53 PM
+ */
+
+namespace UniproBundle\Entity;
+
+
+class User
+{
+
+}
---
+++ src/UniproBundle/UniproBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: buddhika
+ * Date: 8/21/15
+ * Time: 12:48 PM
+ */
+
+namespace UniproBundle;
+
+
+class UniproBundle
+{
+
+}
