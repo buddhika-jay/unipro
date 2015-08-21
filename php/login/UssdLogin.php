051ec734ae4638f30df7ce69d0af444d748f7b5d
AselaDarshan <aseladarshan@gmail.com>
2015-08-21 23:22:18 +0530

    update ussdlogin

--- php/login/UssdLogin.php
+++ php/login/UssdLogin.php
@@ -6,12 +6,12 @@
  * Date: 8/20/15
  * Time: 5:14 PM
  */
-echo "start";
+
 //namespace login;
 include_once '../libs/ussd/MoUssdReceiver.php';
 include_once '../libs/ussd/MtUssdSender.php';
 include_once '../libs/log.php';
-echo "include";
+
 ini_set('error_log', 'ussd-app-error.log');
 
 $receiver = new MoUssdReceiver(); // Create the Receiver object
@@ -29,49 +29,33 @@ $version = $receiver->getVersion(); // get the version
 $sessionId = $receiver->getSessionId(); // get the session ID;
 $ussdOperation = $receiver->getUssdOperation(); // get the ussd operation
 
+$category;
+
 logFile("[ content=$content, address=$address, requestId=$requestId, applicationId=$applicationId, encoding=$encoding, version=$version, sessionId=$sessionId, ussdOperation=$ussdOperation ]");
 
 //your logic goes here......
 $responseMsg = array(
-    "main" => "1.Company
-                    2.Products
-                    3.Careers
-                    000.Exit",
-    "company" => "Company Details
-                    1.CEO
-                    2.Location
-                    3.Branches
-                    4.Contact
-                    999.Back",
-    "products" => "Products
-                    1.SDP
-                    2.Soltura
-                    999.Back",
-    "careers" => "Careers
-                    1.Software Engineer
-                    2.Project Manager
-                    999.Back",
-    "ceo" => "Mr.Dinesh Saparamadu.
-                    999.Back",
-    "location" => "Nawam mawatha,Colombo, Srilanka.
-                    999.Back",
-    "branches" => "hSenid mobile
-                    999.Back",
-    "contact" => "info@hsenid.com
-                    999.Back",
-    "sdp" => "Career grade service delivery platform
-                    999.Back",
-    "soltura" => "Service Creation Environment
-                    999.Back",
-    "software-engineer" => "Java Software Engineer for mobile back end development ( email: careers@hsenidmobile.com )
-                    999.Back",
-    "project-manager" => "Experienced Project Manager ( email: careers@hsenidmobile.com )
-                    999.Back"
+    "main" => "Welcome!
+                    1.Register
+                    2.Jobs
+                    99.Exit",
+    "register" => "Please select a category
+                    1.Tech
+                    2.Business
+                    3.Entertainment
+                    99.Back",
+    "jobs"=> "You have received following jobs",
+    "selected" =>"You selected
+                        1.Confirm registration
+                    99. Back",
+    "registered" => "Thank you for register in our service
+                      Now you will receive jobs in selected category "
 );
 
 logFile("Previous Menu is := " . $_SESSION['menu-Opt']); //Get previous menu number
 if (($receiver->getUssdOperation()) == "mo-init") { //Send the main menu
-    loadUssdSender($sessionId, $responseMsg["main"]);
+    //loadUssdSender($sessionId, $responseMsg["main"]);
+    send($sessionId, $responseMsg["main"],$address);
     if (!(isset($_SESSION['menu-Opt']))) {
         $_SESSION['menu-Opt'] = "main"; //Initialize main menu
     }
@@ -84,10 +68,10 @@ if (($receiver->getUssdOperation()) == "mo-cont") {
         case "main":
             switch ($receiver->getMessage()) {
                 case "1":
-                    $menuName = "company";
+                    $menuName = "register";
                     break;
                 case "2":
-                    $menuName = "products";
+                    $menuName = "jobs";
                     break;
                 case "3":
                     $menuName = "careers";
@@ -98,20 +82,17 @@ if (($receiver->getUssdOperation()) == "mo-cont") {
             }
             $_SESSION['menu-Opt'] = $menuName; //Assign session menu name
             break;
-        case "company":
-            $_SESSION['menu-Opt'] = "company-hist"; //Set to company menu back
+        case "register":
+            $_SESSION['menu-Opt'] = "register-hist"; //Set to company menu back
             switch ($receiver->getMessage()) {
                 case "1":
-                    $menuName = "ceo";
+                    $_SESSION['cat']="1";
                     break;
                 case "2":
-                    $menuName = "location";
+                    $_SESSION['cat']="2";
                     break;
                 case "3":
-                    $menuName = "branches";
-                    break;
-                case "4":
-                    $menuName = "contact";
+                    $_SESSION['cat']="3";
                     break;
                 case "999":
                     $menuName = "main";
@@ -121,47 +102,25 @@ if (($receiver->getUssdOperation()) == "mo-cont") {
                     $menuName = "main";
                     break;
             }
+            $menuName = "selected";
+            $_SESSION['menu-Opt'] = $menuName;
             break;
-        case "products":
-            $_SESSION['menu-Opt'] = "products-hist"; //Set to product menu back
+
+        case "selected":
             switch ($receiver->getMessage()) {
                 case "1":
-                    $menuName = "sdp";
+                    $menuName = "registered";
+                    //add to database
+                    //_SESSION['cat']
+                    //$address
                     break;
-                case "2":
-                    $menuName = "soltura";
-                    break;
-                case "999":
-                    $menuName = "main";
-                    $_SESSION['menu-Opt'] = "main";
-                    break;
-                default:
-                    $menuName = "main";
-                    break;
+                case "99":
+                    $menuName = "register";
             }
-            break;
-        case "careers":
-            $_SESSION['menu-Opt'] = "careers-hist"; //Set to career menu back
-            switch ($receiver->getMessage()) {
-                case "1":
-                    $menuName = "software-engineer";
-                    break;
-                case "2":
-                    $menuName = "project-manager";
-                    break;
-                case "999":
-                    $menuName = "main";
-                    $_SESSION['menu-Opt'] = "main";
-                    break;
-                default:
-                    $menuName = "main";
-                    break;
-            }
-            break;
-        case "company-hist" || "products-hist" || "careers-hist":
+        case "register-hist" || "products-hist" || "careers-hist":
             switch ($_SESSION['menu-Opt']) { //Execute menu back sessions
-                case "company-hist":
-                    $menuName = "company";
+                case "register-hist":
+                    $menuName = "register";
                     break;
                 case "products-hist":
                     $menuName = "products";
@@ -169,6 +128,8 @@ if (($receiver->getUssdOperation()) == "mo-cont") {
                 case "careers-hist":
                     $menuName = "careers";
                     break;
+                case "selected-hist":
+                    $menuName = "selected";
             }
             $_SESSION['menu-Opt'] = $menuName; //Assign previous session menu name
             break;
@@ -187,7 +148,9 @@ if (($receiver->getUssdOperation()) == "mo-cont") {
     }
     else {
         logFile("Selected response message := " . $responseMsg[$menuName]);
-        $response = loadUssdSender($sessionId, $responseMsg[$menuName]);
+        logFile("cat".$_SESSION['cat']);
+        //$response = loadUssdSender($sessionId, $responseMsg[$menuName]);
+        $response = send($sessionId, $responseMsg[$menuName],$address);
     }
 
 }
@@ -195,6 +158,32 @@ if (($receiver->getUssdOperation()) == "mo-cont") {
     Get the session id and Response message as parameter
     Create sender object and send ussd with appropriate parameters
 **/
+function send($sessionId,$responseMessage,$destinationAddress){
+    $password = "password";
+    if ($responseMessage == "000") {
+        $ussdOperation = "mt-fin";
+    } else {
+        $ussdOperation = "mt-cont";
+    }
+    $chargingAmount = "5";
+    $applicationId = "APP_000001";
+    $encoding = "440";
+    $version = "1.0";
+
+    try {
+        // Create the sender object server url
+
+//        $sender = new MtUssdSender("http://localhost:7000/ussd/send/");   // Application ussd-mt sending http url
+        $sender = new MtUssdSender("https://localhost:7443/ussd/send/"); // Application ussd-mt sending https url
+        $response = $sender->ussd($applicationId, $password, $version, $responseMessage,
+            $sessionId, $ussdOperation, $destinationAddress, $encoding, $chargingAmount);
+        return $response;
+    } catch (UssdException $ex) {
+        //throws when failed sending or receiving the ussd
+        error_log("USSD ERROR: {$ex->getStatusCode()} | {$ex->getStatusMessage()}");
+        return null;
+    }
+}
 
 function loadUssdSender($sessionId, $responseMessage)
 {
